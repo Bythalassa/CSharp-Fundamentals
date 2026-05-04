0c703d2e4f449532323e323c6773358554e3f8bf
Daniel Dentone <ddanielrafael8@gmail.com>
2026-05-04 11:56:13 -0500

    Exercise Two.cs Updated

--- week5/ExerciseTwo.cs
+++ week5/ExerciseTwo.cs
@@ -9,10 +9,7 @@ public class W5PracticalB3 : MonoBehaviour
 
     int[] numbers = new int[3];
 
-    [SerializeField] private List<string> CreaturesNames = new List<string>();
-
-    [SerializeField] private List<string> names = new List<string>();
-
+    [SerializeField] private List<string> colores = new List<string>();
 
     void Start()
     {
@@ -24,6 +21,7 @@ public class W5PracticalB3 : MonoBehaviour
         HigherI();
         equalityVerification();
         replaceValues();
+        HigherIv2();
 
     }
 
@@ -48,9 +46,21 @@ public class W5PracticalB3 : MonoBehaviour
 
     void replaceValues()
     {
-        //verifica si el primer y último elemento del array son iguales
-        string equalityVerification = numbers[0] == numbers[numbers.Length - 1] ? "The first and last element are equal values." : "The first and last element are different values.";
-        print(equalityVerification);
+        //Intercambia los valores de la posicion 0 y la posicion 2 del array
+        int temp = numbers[0];
+        numbers[0] = numbers[2];
+        numbers[2] = temp;
+        print("Values at positions 0 and 2 have been swapped to " + numbers[0] + " and " + numbers[2]);
     }
+
+    void HigherIv2()
+    {
+        //en-desarrollo
+        //muestra cual es el mayor entre posicion 0 y la 1
+        string HigherIv2 = numbers[0] > numbers[2] ? "Index 0 is the highest value" : "Index 1 is the highest value";
+        print(HigherIv2);
+    }
+
+
 
 }
