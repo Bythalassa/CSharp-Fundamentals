cb3dfa208b2986fe3fb7815054eb8043f6801ccc
Daniel Dentone <143889531+Bythalassa@users.noreply.github.com>
2026-05-30 18:04:54 -0500

    file updated

--- week6/ExerciseThree.cs
+++ week6/ExerciseThree.cs
@@ -1,14 +1,17 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using static cicloUno;
+using static UnityEditor.Progress;
 
-public class W6PracticalB4 : MonoBehaviour
-{
+public class cicloUno : MonoBehaviour
+{ //block 3
+    string[] score = new string[4];
+    int[] damageImpact = new int[4];
+    string[] swords = new string[3];
 
-    //Block 4 : Classes (Coleccions)
-
-
-    [System.Serializable] // la clase es identificada de esta manera por Unity para volver publica una lista Personalizada.
-    public class Enemy
+    [Serializable] public class Enemy
     {
         public string name;
         public int health;
@@ -16,83 +19,130 @@ public class W6PracticalB4 : MonoBehaviour
 
     public List<Enemy> enemies = new List<Enemy>();
 
-   
+
     void Start()
     {
-        // Cada método crea su Enemy, asigna la propiedad de nombre y vida, lo imprime y lo entrega a la lista.
-        enemies.Add(propertiesSkullomats()); //a estos los matas de 1 pero son ruidosos
-        enemies.Add(propertiesWretchedWitches()); //ellas tienen 1/3 más de vida pero igual faciles de matar
-        enemies.Add(propertiesWretchedThugs()); //doble de vida que witches, consigue 15% de mejora en cast y ya.
-        enemies.Add(propertiesMegaera()); //Boss 1, tiene vida regular, mucha más resistencia que tú al inicio.
-        enemies.Add(propertiesBrimstone());//el menor de vida pero te arruina la partida si lo evitas.
+        score[0] = "20 Points";
+        score[1] = "45 Points";
+        score[2] = "20 Points";
+        score[3] = "1020 Points";
 
-        recibirDaño("Megaera", 60);
-        recibirDaño("Brimstone", 22);
-        print(CompareHealth(enemies[3], enemies[4]));
+        damageImpact[0] = 100;
+        damageImpact[1] = 80;
+        damageImpact[2] = 40;
+        damageImpact[3] = 15;
 
+        enemies.Add(propertiesSkullomats()); 
+        enemies.Add(propertiesWretchedWitches()); 
+        enemies.Add(propertiesWretchedThugs()); 
+        enemies.Add(propertiesMegaera());
+
+        swords[0] = "blue Sword";
+        swords[1] = "Legendary Sword";
+        swords[2] = "Boss Sword";
+
+        numberedInventory();
     }
 
-    void Update() { }
+    public void showScore()
+    {
+        int i = 0;
+        while (i < score.Length)
+        {
+            Debug.Log(score[i]);
+            i++;
+        }
+    }
+    
+    public void calculateTotalDamage()
+    {
+        int i = 0;
+        int a = 40; //daño total recibido
 
+        do
+        {
+            a = a + damageImpact[i];  
+            Debug.Log("total damage is " + a);
+            i++;
+        }
+        while (i < damageImpact.Length);
+    }
     Enemy propertiesSkullomats()
     {
         Enemy enemy = new Enemy { name = "Skullomats", health = 30 };
-        print("Enemy: " + enemy.name + ", total life: " + enemy.health);
         return enemy;
     }
 
     Enemy propertiesWretchedWitches()
     {
         Enemy enemy = new Enemy { name = "Wretched Witches", health = 55 };
-        print("Enemy: " + enemy.name + ", total life: " + enemy.health);
         return enemy;
     }
 
     Enemy propertiesWretchedThugs()
     {
-        Enemy enemy = new Enemy { name = "Wretched Thugs", health = 150 };
-        print("Enemy: " + enemy.name + ", total life: " + enemy.health);
+        Enemy enemy = new Enemy { name = "Wretched Thugs", health = 0 };
         return enemy;
     }
 
     Enemy propertiesMegaera()
     {
-        Enemy enemy = new Enemy { name = "Megaera", health = 750 };
-        print("Enemy: " + enemy.name + ", total life: " + enemy.health);
+        Enemy enemy = new Enemy { name = "Megaera", health = 0 };
         return enemy;
     }
-
-    Enemy propertiesBrimstone()
+    public void showAliveEnemy()
     {
-        Enemy enemy = new Enemy { name = "Brimstone", health = 10 };
-        print("Enemy: " + enemy.name + ", total life: " + enemy.health);
-        return enemy;
-    }
+        int i = 0;
 
-    void recibirDaño(string enemyName, int damage) /*parámetros se llaman desde el Start,
-                                                   se le pasa el nombre del enemigo y la cantidad de daño a recibir.
-                                                   mientras recibirDaño se llame, los enemigos recibiran la actualización.*/
-    {
-        Enemy target = enemies.Find(e => e.name == enemyName); // Busca por nombre
-        /*Enemy target: Declara una variable llamada target de tipo Enemy
-        .Find(...) es Método de List<T> que recorre la lista y devuelve el primer elemento que cumpla la condición dada.  
-        (e => e.name == enemyName) : "dado un enemigo e, comprueba si e.name es igual a enemyName", pero yo no entiendo esto.*/
-        if (target != null)
-        { // != es (diferente de), es decir "si target no es nulo, entonces..."
-            target.health -= damage;
-            print(target.name + " received damage: " + damage + ", remaining life: " + target.health);
-        }
-        else
-        { 
-            print("Enemy '" + enemyName + "' not found.");
+        while (i < enemies.Count)
+        {
+            if (enemies[i].health > 0)
+            {
+                Debug.Log(enemies[i].name);
+            }
+            i++;
         }
     }
 
-    string CompareHealth(Enemy enemy4, Enemy enemy5)
+    /*El while controla cuántas veces iteras → tiene que ser i < enemies.Count
+     El if controla qué se muestra → ahí va el filtro health > 0
+     es imporante recorrer toda la lista para no cortar a enemigos vivos nombrados luego de 1 muerto (enemyHealth = 0). */
+    public void findWeirdItem()
     {
-        return enemy4.health > enemy5.health 
-            ? enemy4.name + " has more health than " + enemy5.name 
-            : enemy5.name + " has more health than " + enemy4.name;
+        int a = 0;
+        do
+        {
+            if (swords[a] != "Legendary Sword")
+            {
+                Debug.Log(swords[a] + " is not a fit ");
+            }
+            else
+            {
+                Debug.Log("found " + swords[a]);
+            }
+                a++;
+        }
+        while (a < swords.Length);
     }
+
+
+    public void numberedInventory()
+    {
+        int a = 0;
+        do
+        {
+            Debug.Log(swords[a] + " (" + a + ")");
+
+            a++;
+        }
+        while (a < swords.Length);
+    }
+
+
 
 }
+
+
+
+
+
